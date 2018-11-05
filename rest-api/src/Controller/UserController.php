f60fd06c8d3406d1e48e0200c02b5cca1d48777d
Leo <leorojas22@gmail.com>
2018-11-04 21:38:30 -0500

    part 1

---
+++ rest-api/src/Controller/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/register", name="api_register", methods={"POST"})
+     */
+    public function register(ObjectManager $om, UserPasswordEncoderInterface $passwordEncoder, Request $request)
+    {
+
+        $user = new User();
+
+        $email                  = $request->request->get("email");
+        $password               = $request->request->get("password");
+        $passwordConfirmation   = $request->request->get("password_confirmation");
+
+        $errors = [];
+        if($password != $passwordConfirmation)
+        {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+
+        if(strlen($password) < 6)
+        {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+
+        if(!$errors)
+        {
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+
+            try
+            {
+                $om->persist($user);
+                $om->flush();
+
+                return $this->json([
+                    'user' => $user
+                ]);
+            }
+            catch(UniqueConstraintViolationException $e)
+            {
+                $errors[] = "The email provided already has an account!";
+            }
+            catch(\Exception $e)
+            {
+                $errors[] = "Unable to save new user at this time.";
+            }
+
+        }
+
+
+        return $this->json([
+            'errors' => $errors
+        ], 400);
+
+    }
+
+    /**
+     * @Route("/login", name="api_login", methods={"POST"})
+     */
+    public function login()
+    {
+        return $this->json(['result' => true]);
+    }
+
+    /**
+     * @Route("/profile", name="api_profile")
+     * @IsGranted("ROLE_USER")
+     */
+    public function profile()
+    {
+        return $this->json([
+            'user' => $this->getUser()
+        ], 200, [], [
+            'groups' => ['api']
+        ]);
+    }
+
+
+    /**
+     * @Route("/", name="api_home")
+     */
+    public function home()
+    {
+        return $this->json(['result' => true]);
+    }
+
+}
---
+++ rest-api/src/Security/LoginAuthenticator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class LoginAuthenticator extends AbstractGuardAuthenticator
+{
+
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->get("_route") === "api_login" && $request->isMethod("POST");
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return [
+            'email' => $request->request->get("email"),
+            'password' => $request->request->get("password")
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $userProvider->loadUserByUsername($credentials['email']);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new JsonResponse([
+            'error' => $exception->getMessageKey()
+        ], 400);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new JsonResponse([
+            'result' => true
+        ]);
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse([
+            'error' => 'Access Denied'
+        ]);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
